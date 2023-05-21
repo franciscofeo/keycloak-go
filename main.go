50d52e292c3e16fefca96b38c6ae362f18b8e9f8
Francisco Angelo <franciscoangelo.feo@gmail.com>
2023-05-21 03:06:06 -0300

    feat: create function to handle /auth/callback endpoint

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"context"
 	oidc "github.com/coreos/go-oidc"
 	"golang.org/x/oauth2"
+	json2 "gopkg.in/square/go-jose.v2/json"
 	"log"
 	"net/http"
 )
 
 var (
 	clientID     = "myclient"
-	clientSecret = "4bcvizadUa0Q19YJMcsX2CXvTdbusIbS"
+	clientSecret = "UTyzwTKqqMzFwlj0CLzrGhbPtQkAtVe6"
 )
 
 func main() {
@@ -33,6 +34,33 @@ func main() {
 
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		http.Redirect(writer, request, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/callback", func(writer http.ResponseWriter, request *http.Request) {
+		if request.URL.Query().Get("state") != state {
+			http.Error(writer, "Invalid state", http.StatusInternalServerError)
+			return
+		}
+
+		token, err := config.Exchange(ctx, request.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(writer, "Cannot exchange the code token", http.StatusInternalServerError)
+			return
+		}
+
+		response := struct {
+			AccessToken *oauth2.Token
+		}{
+			token,
+		}
+
+		json, err := json2.Marshal(response)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		writer.Write(json)
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
